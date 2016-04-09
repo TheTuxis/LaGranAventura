02f0409b4caecca60e3a06d86cc1e1cb4aeebcee
Pablo N Dalmasso <pablodalmasso@gmail.com>
2016-04-09 19:23:16 -0300

    LAG-5 Se sube el formulario de la app book

---
+++ src/apps/books/forms.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from django import forms
+from apps.books.models import Book, Page, Option, PlayerSection, PlayerSectionDetail
+
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ('name', 'description', 'pic', 'published')
+
+
+class PageForm(forms.ModelForm):
+    class Meta:
+        model = Page
+        fields = ('book', 'name', 'description', 'pic', 'page_number')
+
+
+class OptionForm(forms.ModelForm):
+    class Meta:
+        model = Option
+        fields = (
+            'page', 'name', 'description', 'order', 'pic', 'px', 'next_page'
+        )
+
+
+class PlayerSectionForm(forms.ModelForm):
+    class Meta:
+        model = PlayerSection
+        fields = ('player', 'book', 'actual_page', 'px')
+
+
+class PlayerSectionDetailForm(forms.ModelForm):
+    class Meta:
+        model = PlayerSectionDetail
+        fields = (
+            'player_section', 'actual_page', 'option', 'px'
+        )
