de4c6b36c8fd9dd85836dc59a4ee9bb1f36fa287
Pablo N Dalmasso <pablodalmasso@gmail.com>
2016-04-09 22:48:00 -0300

    LAG-9 Se resuelve issue

--- src/LaGranAventura/urls.py
+++ src/LaGranAventura/urls.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """LaGranAventura URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    url(r'^', include('apps.core.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', auth_views.login),
+    url(r'^accounts/logout/$', auth_views.logout),
 ]
